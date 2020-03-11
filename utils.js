b3c80b444595fc6790c29249bb9b2ccfba8d1964
Akshay Nanda <akshayn1388@gmail.com>
2020-03-11 15:13:04 -0700

    utils

---
+++ utils.js
@@ -0,0 +1,16 @@
+function add(a,b){
+
+    return a+b;
+}
+function multiply(a,b){
+
+    return a*b;
+}
+function power(a,b){
+    return (Math.pow(a,b));
+}
+function substract(a,b){s
+
+    return a-b;
+}
+
